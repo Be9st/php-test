7adcbeb2a05960bc6dfb188fa63b80c30b27c835
Be9st <sasha30402@mail.ru>
2019-04-14 23:20:40 +0300

    first

---
+++ PHP/block-header.php
@@ -0,0 +1,16 @@
+<div id="block-header">
+    <div id="header-top-block">
+        <ul id="header-top-menu">
+            <li>Ваш город - <span>Москва</span></li>
+            <li><a href="about.php">О нас</a></li>
+            <li><a href="shops.php">Магазины</a></li>
+            <li><a href="contacts.php">Контакты</a></li>
+        </ul>
+
+        <p id="reg-auth-title" align="right">
+            <a class="top-auth" href="#">Вход</a>
+            <a href="registration.php">Регистрация</a>
+        </p>
+    </div>
+    <hr id="header-line">
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="windows-1251">
+    <title>Km</title>
+    <link href="CSS/styles.css" rel="stylesheet" type="text/css">
+    <link href="CSS/reset.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="block-body">
+
+    <?php include("PHP/block-header.php") ?>
+    <div id="block-right"></div>
+    <div id="block-content"></div>
+    <?php include("PHP/block-footer.php") ?>
+
+</div>
+</body>
+</html>
